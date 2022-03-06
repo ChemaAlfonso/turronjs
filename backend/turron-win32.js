7d8ced29cc815c564ef3b7c3f452eac98baac477
Chema Alfonso <hola@chemaalfonso.com>
2022-03-06 16:40:51 +0100

    feature: reunTurron on win32 executing new scripts

--- backend/turron-win32.js
+++ backend/turron-win32.js
@@ -3,6 +3,8 @@ const path          = require('path')
 const { TurronJs }  = require('./turron')
 
 class TurronJsWin32 extends TurronJs {
+    win32TurronShFileName = path.join(__dirname, '/win32/turronsh/turron.ps1')
+    win32TurronAudioShFileName = path.join(__dirname, '/win32/turronsh/turron-audio.ps1')
 
     constructor( window ) {
         super( window )
@@ -11,8 +13,19 @@ class TurronJsWin32 extends TurronJs {
     // ===================================
     // Turron actions
     // ===================================
-    runTurron() {
-        console.log('Turron will be processed')
+    runTurron( audio = false ) {
+
+        runScript(this.activeWindow, 'powershell Set-ExecutionPolicy RemoteSigned ', null);
+
+        this.runTurronBySh( audio )
+
+        runScript(this.activeWindow, 'powershell Set-ExecutionPolicy Restricted ', null);
+        
+    }
+
+    runTurronBySh( audio ) {
+        const targetCommandPath = audio ? this.win32TurronAudioShFileName : this.win32TurronShFileName
+        runScript(this.activeWindow, 'powershell ' + targetCommandPath, null);
     }
 
 
